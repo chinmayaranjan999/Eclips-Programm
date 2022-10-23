8e5dc26d176e12a08fc9377e297bcd0d2055961a
chinmay <chinmaya.ranjan999@gmail.com>
2022-10-23 11:37:18 +0530

    First commit

---
+++ Java/src/demo/GitLession.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class GitLession {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Dinesh Kumar");
+	}
+
+}
---
+++ Java/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author chinm
+ *
+ */
+module Java {
+}
